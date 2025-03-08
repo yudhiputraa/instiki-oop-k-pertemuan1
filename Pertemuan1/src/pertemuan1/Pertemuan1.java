845f97d7737533bd1a2e5745bd0b1a24cd5de158
Yudhi Putra <putuyudhi1@gmail.com>
2025-03-08 12:18:46 +0800

    Keterangan commit

---
+++ Pertemuan1/src/pertemuan1/Pertemuan1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan1;
+
+/**
+ *
+ * @author Yudhi Putra
+ * TGL 8 Maret 2025
+ */
+public class Pertemuan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // menampilkan tulisan di layar
+        System.out.print("Latihan Bahasa Java");
+    }
+    
+}
